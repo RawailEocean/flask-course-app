5059e04b2b64060f95ee75d04637eb4ea209850d
RawailEocean <rawail.ahmed@eocean.net>
2025-07-14 01:14:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template
 import redis
-import os
 
 app = Flask(__name__)
-redis_host = os.getenv("REDIS_HOST", "redis")
-r = redis.Redis(host=redis_host, port=6379)
+r = redis.Redis(host='redis', port=6379)
 
-@app.route("/")
+@app.route('/')
 def home():
-    count = r.incr("hits")
-    return render_template("index.html", count=count)
+    count = r.incr('hits')
+    return render_template('index.html', count=count)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
